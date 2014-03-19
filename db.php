c604453de92bc2262e64199eb6acea32865fcd37
Eric Bower <neurosnap@gmail.com>
2014-03-18 23:45:09 -0400

    bleh

--- db.php
+++ db.php
@@ -5,10 +5,10 @@
 		this file will normally be ignored from git
 	*/
 
-	$server = "www.nysus.net";
-	$db = "eric_dev";
-	$user = "nysususer";
-	$pass = "nysus2444";
+	$server = "";
+	$db = "";
+	$user = "";
+	$pass = "";
 	//e.g. sqlsrv, mssql, mysql
 	$driver = "sqlsrv";
 
